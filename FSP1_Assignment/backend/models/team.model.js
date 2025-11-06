718cefcb1b05792d39338fcbb0d202ca1913100a
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2025-11-06 10:51:33 +0530

    Deleting Files

--- FSP1_Assignment/backend/models/team.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-// Team Schema 
-const teamSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },  // Team names must be unique 
-});
-module.exports = mongoose.model('Team', teamSchema);
--- FSP1_Assignment/frontend/src/pages/ProjectForm.jsx
+++
@@ -1,7 +0,0 @@
-function ProjectForm() {
-    return ( 
-        <>ProjectForm</>
-     );
-}
-
-export default ProjectForm;
