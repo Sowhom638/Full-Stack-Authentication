14f97c6879a66ed873f791ac6c41c1b21f21d301
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2025-11-08 11:53:33 +0530

    FSP2.5

---
+++ FSP2.1/errorLogger.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+function logError(message) {
+  const errorFilePath = path.resolve("./logs/error.log");
+  fs.appendFile(
+    errorFilePath,
+    `${new Date().toISOString()}: ${message}\n`,
+    (err) => {
+      if (err) {
+        console.error("failed to log error", err);
+      }
+    }
+  );
+}
+
+module.exports = logError;
---
+++ FSP2.1/utils/fileChecker.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const fs = require("fs");
+
+const path = process.env.FILE_PATH;
+const logError = require("../errorLogger.js")
+
+function checkAndCreateFile(filePath) {
+  if (!filePath) {
+    console.log("File path is not defined.");
+    return;
+  }
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.writeFile(filePath, "Sample Text", (writeErr) => {
+        if (writeErr) logError(`Error creating file ${writeErr}`);
+        else console.log("File Created");
+      });
+    }else{
+        console.log("File already exists");       
+    }
+  });
+}
+checkAndCreateFile(path)
