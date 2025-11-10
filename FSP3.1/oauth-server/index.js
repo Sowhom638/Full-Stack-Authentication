3e7d72459ebe45897672546b9a50ad58fbb657d1
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2025-11-10 23:14:58 +0530

    FSP3.2 Done

--- FSP3.1/oauth-server/index.js
+++ FSP3.1/oauth-server/index.js
@@ -6,33 +6,37 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.json(`<h1>Welcome to OAuth API server.</h1>`)
 })
 
-app.get("/auth/github", (req, res)=>{
+// GitHub OAuth
+app.get("/auth/github", (req, res) => {
     const gitHubUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user,repo,security_events`;
     res.redirect(gitHubUrl);
 })
 
-app.get("/auth/github/callback",async (req, res)=>{
-    const {code} = req.query;
+app.get("/auth/github/callback", async (req, res) => {
+    const { code } = req.query;
+    if (!code) {
+        return res.status(400).send("Authorization code not provided");
+    }
     try {
-        const tokenResponse = await axios.post(`https://github.com/login/oauth/access_token`,{
+        const tokenResponse = await axios.post(`https://github.com/login/oauth/access_token`, {
             client_id: process.env.GITHUB_CLIENT_ID,
             client_secret: process.env.GITHUB_CLIENT_SECRET,
             code,
-        },{
-            headers: {Accept: "application/json"}
+        }, {
+            headers: { Accept: "application/json" }
         })
-        
+
         const accessToken = tokenResponse.data.access_token;
         res.cookie("access_token", accessToken);
         return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/github`);
@@ -41,6 +45,42 @@ app.get("/auth/github/callback",async (req, res)=>{
     }
 })
 
-app.listen(PORT,()=>{
+
+// Google OAuth
+app.get("/auth/google", (req, res) => {
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+    res.redirect(googleAuthUrl);
+})
+app.get("/auth/google/callback", async (req, res) => {
+    const { code } = req.query;
+    if (!code) {
+        return res.status(400).send("Authorization code not provided");
+    }
+
+    let accessToken;
+    try {
+        const tokenResponse = await axios.post("https://oauth2.googleapis.com/token", 
+            new URLSearchParams({
+                client_id: process.env.GOOGLE_CLIENT_ID,
+                client_secret: process.env.GOOGLE_CLIENT_SECRET,
+                code,
+                grant_type: "authorization_code",
+                redirect_uri: `http://localhost:${PORT}/auth/google/callback`
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            }
+        )
+        accessToken = tokenResponse.data.access_token;
+        res.cookie("access_token", accessToken);
+        return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+app.listen(PORT, () => {
     console.log(`Server is running http://localhost:${PORT}`);
 });
